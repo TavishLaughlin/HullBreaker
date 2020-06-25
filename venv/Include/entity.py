9c3a96833f9444378c8e07f66d3fae4f55784b6c
TavishLaughlin <tavishlaughlin@gmail.com>
2020-06-24 17:19:06 -0700

    Initial commit

---
+++ venv/Include/entity.py
@@ -0,0 +1,15 @@
+from typing import Tuple
+
+
+class Entity:
+    # a generic object to represent players, enemies, items
+
+    def __init__(self, x: int, y: int, char: str, color: Tuple[int, int, int]):
+        self.x = x
+        self.y = y
+        self.char = char
+        self.color = color
+    def move(selfself, dx: int, dy: int) -> None:
+        # move entity by given amount
+        self.x += dx
+        self.y += dy
